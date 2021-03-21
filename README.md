d9fa35d877b9011fb4c0bdeda47d5f3d7a67e0ee
Eric Burden <eric.w.burden@gmail.com>
2021-03-20 22:19:18 -0500

    Deploy to Netlify
    Use the netlify link, change site folder from "docs" to "site"

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A presentation given at [CodeConnector's](https://codeconnector.io/) 2021-03-23 Lightning Talks focused on R and R Markdown for Literate Programming. You can view the presented material [here](https://ericwburden.github.io/20210323-literate-programming-talk/index.html).
+A presentation given at [CodeConnector's](https://codeconnector.io/) 2021-03-23 Lightning Talks focused on R and R Markdown for Literate Programming. You can view the presented material [here](https://literate-programming.ericburden.page).
--- build.R
+++ build.R
@@ -1,19 +1,19 @@
 invisible(file.remove("_main.Rmd")) # Clean up in case previous build failed
-unlink("docs/figures", recursive = T)
+unlink("site/figures", recursive = T)
 
 # Renders the book to HTML
-bookdown::render_book("_bookdown.yml", "bookdown::gitbook", output_dir = "docs")
+bookdown::render_book("_bookdown.yml", "bookdown::gitbook", output_dir = "site")
 
 # Post-Build Steps -------------------------------------------------------------
 
-file.rename("docs/presenting.html", "docs/index.html") # The root page
+file.rename("site/presenting.html", "site/index.html") # The root page
 
 # GitHub pages apparently doesn't like directories that start with underscores,
 # so replacing "_main_fils" with "figures". Rename directory and replace in
 # paths in ths *.html files
 
-file.rename("docs/_main_files", "docs/figures")
-html_files <- list.files("docs", "*.html", full.names = T)
+file.rename("site/_main_files", "site/figures")
+html_files <- list.files("site", "*.html", full.names = T)
 
 for (html_file in html_files) {
   file_contents <- readr::read_file(html_file)
